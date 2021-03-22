ed2a434faa9b26ca077e262add9bbee553f87b46
Robin de Zwart <robindezwart11@gmail.com>
2021-03-22 11:16:11 -0700

    Complete Step 4

--- Part1/main.py
+++ Part1/main.py
@@ -6,11 +6,12 @@
 # -- Imports -- #
 
 import random
+from sklearn.linear_model import LinearRegression
 
 
 # -- Functions -- #
 
-def generate_training_set(t_min, t_max):
+def generate_training(t_min, t_max):
     ret = []
     for i in range(100):
         ret.append([
@@ -21,8 +22,32 @@ def generate_training_set(t_min, t_max):
     return ret
 
 
+def process_training(training):
+    """
+    Formula: y = (1 * x1) + (2 * x2) + (3 * x3)
+    :param training: training_set, 2D array
+    :return: output_set, 1D array
+    """
+    ret = []
+    for i in range(len(training)):
+        val = 0
+        for j in range(3):
+            val += (j + 1) * training[i][j]
+        ret.append(val)
+    return ret
+
+
 # -- Code -- #
 
-training_set = generate_training_set(0, 1000)
+train_input = generate_training(0, 1000)
+train_output = process_training(train_input)
 
-print(training_set)
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=train_input, y=train_output)
+
+X_test = [[10, 20, 30]]
+outcome = predictor.predict(X=X_test)
+coefficients = predictor.coef_
+
+print("Prediction: " + str(outcome))
+print("Coefficients: " + str(coefficients))
