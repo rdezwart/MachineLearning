fb776cb354e841bb1abc6d9bd8cc663845939e0a
Robin de Zwart <robindezwart11@gmail.com>
2021-04-07 15:59:10 -0700

    Revision pass
    Updated comments/formatting, added docstrings and relevant type hints.

--- Part1/main.py
+++ Part1/main.py
@@ -1,18 +1,26 @@
 # Title: Final Project (Machine Learning for Prediction) - Part 1
 # Authors: Robin de Zwart, Veronika Tatsiy, MachineLearning42
-# Date: Mar 22, 2021
-# Purpose: Generating a training set
+# Date: Apr 07, 2021
+# Purpose: Building our first Machine Learning Model
 
-# 1. Imports 
+# -- Imports -- #
 
 import random
+
 from sklearn.linear_model import LinearRegression
 
 
-# 2. Generate training function 
+# -- Functions -- #
 
-def generate_training(t_min, t_max):
-    ret = []
+def generate_training(t_min: int, t_max: int):
+    """
+    Generates 2D array of training data, with values based on parameters.
+
+    :param t_min: minimum value for training data
+    :param int t_max: maximum value for training data
+    :return: list of lists containing three integers
+    """
+    ret: list[list[int]] = []
     for i in range(100):
         ret.append([
             random.randint(t_min, t_max),
@@ -22,15 +30,16 @@ def generate_training(t_min, t_max):
     return ret
 
 
-# 3. Process training function 
+def process_training(training: list[list[int]]):
+    """
+    Takes 2D list of training data and applies a formula, and returns the results.
 
-def process_training(training):
-    """
     Formula: y = (1 * x1) + (2 * x2) + (3 * x3)
-    :param training: training_set, 2D array
-    :return: output_set, 1D array
+
+    :param training: raw training data
+    :return: processed training data
     """
-    ret = []
+    ret: list[int] = []
     for i in range(len(training)):
         val = 0
         for j in range(3):
@@ -39,19 +48,26 @@ def process_training(training):
     return ret
 
 
-# 4. General code
+# -- Code -- #
 
+# - Step 1 - #
+# Implemented in process_training()
+
+# - Step 2 - #
+# Generate input and output training data
 train_input = generate_training(0, 1000)
 train_output = process_training(train_input)
 
+# - Step 3 - #
+# Train the machine learning model
 predictor = LinearRegression(n_jobs=-1)
 predictor.fit(X=train_input, y=train_output)
 
+# - Step 4 - #
+# Make a prediction with test data
 X_test = [[10, 20, 30]]
 outcome = predictor.predict(X=X_test)
 coefficients = predictor.coef_
-
-# 5. Print statements 
-
+# Final output
 print("Prediction: " + str(outcome))
 print("Coefficients: " + str(coefficients))
